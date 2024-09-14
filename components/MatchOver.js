866033a82fff4f9f3cfd45a7ec5f61bc0a4f3328
sarrah26 <saify.sarrah26@gmail.com>
2024-09-14 17:24:07 +0530

    add log

--- components/MatchOver.js
+++ components/MatchOver.js
@@ -2,13 +2,14 @@
 import styles from './TicTacToe.module.css';
 
 const MatchOver = ({ winner, winningLine }) => {
-  if (!winner) return null;
+    console.log(winner, winningLine)
+    if (!winner) return null;
 
-  return (
-    <div className={styles.status}>
-      {winner.winner === 'Draw' ? 'It\'s a Draw!' : `Winner: ${winner.winner}`}
-    </div>
-  );
+    return (
+        <div className={styles.status}>
+        {winner.winner === 'Draw' ? 'It\'s a Draw!' : `Winner: ${winner.winner}`}
+        </div>
+    );
 };
 
 export default MatchOver;
